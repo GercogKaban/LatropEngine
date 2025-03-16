f7fe28c8e53ba625964b9718cea447516930e8f9
Artyom Rudakov <two_dollars@icloud.com>
2025-03-16 03:45:39 +0200

    Connects restitution and friction

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,7 @@ int main()
 	bodyB->m_takesGravity = true;
 	bodyB->m_gravity = { 0, -1, 0 }; //LEngine::get()->physicsWorld.m_gravity;
 	bodyB->m_mass = 1;
+	bodyB->m_restitution = 2;
 	bodyB->transform = new LatropPhysics::Transform();
 	bodyB->transform->position = glm::vec3(0.5f, 10.0f, 0.0f);
 	bodyB->transform->scale = glm::vec3(1.0f, 1.0f, 1.0f);
