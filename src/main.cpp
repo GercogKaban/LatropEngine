1a7714f45486a825c69a96cc04780f76fd59f2b8
Artyom Rudakov <two_dollars@icloud.com>
2025-03-17 02:10:33 +0200

    Adds support for non-kinematic(static) bodies

--- src/main.cpp
+++ src/main.cpp
@@ -20,20 +20,22 @@ int main()
 
 	LEngine engine(wnd);
 
-	auto character = ObjectBuilder::construct(std::make_shared<PlayerCharacter>(nullptr, std::make_shared<LatropPhysics::RigidBody>(false)));
-	auto characterPhyscis = character.lock()->physicsComponent;
-	characterPhyscis->collider = cubeAABBCollider;
+	auto character = ObjectBuilder::construct(std::make_shared<PlayerCharacter>(nullptr, nullptr/*std::make_shared<LatropPhysics::RigidBody>(true)*/));
+	// auto characterPhyscis = character.lock()->physicsComponent;
+	// characterPhyscis->transform.position = glm::vec3(0.0f, 0.0f, 10.0f);
+	// characterPhyscis->collider = cubeAABBCollider;
 
 	auto cubeA = ObjectBuilder::construct(std::make_shared<LActor>(std::make_shared<LG::LCube>(), std::make_shared<LatropPhysics::RigidBody>(false)));
 	auto bodyA = cubeA.lock()->physicsComponent;
 	bodyA->transform.position = glm::vec3(0.0f, 0.0f, 0.0f);
 	bodyA->collider = cubeAABBCollider;
+	bodyA->m_restitution = 2;
 
-	auto cubeB = ObjectBuilder::construct(std::make_shared<LActor>(std::make_shared<LG::LCube>(), std::make_shared<LatropPhysics::RigidBody>(true)));
+	auto cubeB = ObjectBuilder::construct(std::make_shared<LActor>(std::make_shared<LG::LCube>(), std::make_shared<LatropPhysics::RigidBody>(true, true)));
 	auto bodyB = cubeB.lock()->physicsComponent;
-	bodyB->transform.position = glm::vec3(0.5f, 10.0f, 0.0f);
+	bodyB->transform.position = glm::vec3(0.0f, 10.0f, 0.0f);
 	bodyB->collider = cubeAABBCollider;
-	bodyB->m_restitution = 2;
+	bodyB->m_restitution = 1;
 	bodyB->m_mass = 0.95;
 
 	// auto character = ObjectBuilder::construct<PlayerCharacter>(new PlayerCharacter());
