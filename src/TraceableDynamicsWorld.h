93f3c3dbffca8ce5e1bec691838d426988ee9036
Artyom Rudakov <two_dollars@icloud.com>
2025-03-20 23:07:35 +0200

    Adjusts the setup for stress-testing

--- src/TraceableDynamicsWorld.h
+++ src/TraceableDynamicsWorld.h
@@ -3,27 +3,39 @@
 
 namespace LatropPhysics
 {
-    struct TraceableDynamicsWorld : DynamicsWorld 
+    struct TraceableDynamicsWorld final : DynamicsWorld 
     {
-        virtual void resolveCollisions(float deltaTime)
+        virtual void detectCollisions(std::vector<Collision>& collisions, std::vector<Collision>& triggers) override
+        {
+            ZoneScoped;
+            DynamicsWorld::detectCollisions(collisions, triggers);
+        }
+        
+        virtual void resolveCollisions(float deltaTime) override
         {
             ZoneScoped;
             DynamicsWorld::resolveCollisions(deltaTime);
         }
 
-        virtual void integrate(float deltaTime)
+        virtual void solveCollisions(const std::vector<Collision>& collisions, float deltaTime) override
+        {
+            ZoneScoped;
+            DynamicsWorld::solveCollisions(collisions, deltaTime);
+        }
+
+        virtual void integrate(float deltaTime) override
         {
             ZoneScoped;
             DynamicsWorld::integrate(deltaTime);
         }
 
-        virtual void applyGravity()
+        virtual void applyGravity() override
         {
             ZoneScoped;
             DynamicsWorld::applyGravity();
         }
 
-        virtual void moveBodies(float deltaTime)
+        virtual void moveBodies(float deltaTime) override
         {
             ZoneScoped;
             DynamicsWorld::moveBodies(deltaTime);
