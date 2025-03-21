cbc2d72392a4afc6e55f8cd0d439622c48dc55cb
Artyom Rudakov <two_dollars@icloud.com>
2025-03-21 21:41:36 +0200

    Implements SmoothPositionSolver

--- LatropPhysics/src/dynamics/SmoothPositionSolver.cpp
+++ LatropPhysics/src/dynamics/SmoothPositionSolver.cpp
@@ -7,44 +7,44 @@ void SmoothPositionSolver::solve(const std::vector<Collision>& collisions, float
 {
     std::vector<std::pair<glm::vec3, glm::vec3>> deltas;
 
-		// for (Collision& manifold : collisions) {
-		// 	RigidBody* aBody = manifold.ObjA->IsDynamic ? (RigidBody*)manifold.ObjA : nullptr;
-		// 	RigidBody* bBody = manifold.ObjB->IsDynamic ? (RigidBody*)manifold.ObjB : nullptr;
+	for (const Collision& manifold : collisions) {
+		RigidBody* aBody = manifold.bodyA->m_isSimulated ? (RigidBody*)manifold.bodyA : nullptr;
+		RigidBody* bBody = manifold.bodyB->m_isSimulated ? (RigidBody*)manifold.bodyB : nullptr;
 
-		// 	float aInvMass = aBody ? aBody->InvMass : 0.0f;
-		// 	float bInvMass = bBody ? bBody->InvMass : 0.0f;
+		float aInvMass = aBody ? 1 / aBody->m_mass : 1.0f;
+        float bInvMass = bBody ? 1 / bBody->m_mass : 1.0f;
 
-		// 	const float percent = 0.8f;
-		// 	const float slop = 0.01f;
+		const float percent = 0.8f;
+		const float slop = 0.01f;
 
-		// 	glm::vec3 correction = manifold.Normal * percent
-		// 		* fmax(manifold.Depth - slop, 0.0f)
-		// 		/ (aInvMass + bInvMass);
-		
-		// 	glm::vec3 deltaA;
-		// 	glm::vec3 deltaB;
+		glm::vec3 correction = manifold.points.normal * percent
+			* fmax(manifold.points.depth - slop, 0.0f)
+			/ (aInvMass + bInvMass);
+	
+		glm::vec3 deltaA;
+		glm::vec3 deltaB;
 
-		// 	if (aBody ? aBody->IsSimulated : false) {
-		// 		deltaA = aInvMass * correction;
-		// 	}
+		if (aBody ? aBody->m_isSimulated : false) {
+			deltaA = aInvMass * correction;
+		}
 
-		// 	if (bBody ? bBody->IsSimulated : false) {
-		// 		deltaB = bInvMass * correction;
-		// 	}
+		if (bBody ? bBody->m_isSimulated : false) {
+			deltaB = bInvMass * correction;
+		}
 
-		// 	deltas.emplace_back(deltaA, deltaB);
-		// }
+		deltas.emplace_back(deltaA, deltaB);
+	}
 
-		// for (unsigned i = 0; i < manifolds.size(); i++) {
-		// 	RigidBody* aBody = manifolds[i].ObjA->IsDynamic ? (RigidBody*)manifolds[i].ObjA : nullptr;
-		// 	RigidBody* bBody = manifolds[i].ObjB->IsDynamic ? (RigidBody*)manifolds[i].ObjB : nullptr;
+	for (unsigned i = 0; i < collisions.size(); i++) {
+		RigidBody* aBody = collisions[i].bodyA->m_isSimulated ? (RigidBody*)collisions[i].bodyA : nullptr;
+		RigidBody* bBody = collisions[i].bodyB->m_isSimulated ? (RigidBody*)collisions[i].bodyB : nullptr;
 
-		// 	if (aBody ? aBody->IsSimulated : false) {
-		// 		aBody->transform->position -= deltas[i].first;
-		// 	}
+		if (aBody ? aBody->m_isSimulated : false) {
+			aBody->transform.position -= deltas[i].first;
+		}
 
-		// 	if (bBody ? bBody->IsSimulated : false) {
-		// 		bBody->transform->position += deltas[i].second;
-		// 	}
-		// }
+		if (bBody ? bBody->m_isSimulated : false) {
+			bBody->transform.position += deltas[i].second;
+		}
+	}
 }
