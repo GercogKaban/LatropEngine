452aef1f6633780af2f4d298ba3b46ae10f2d1f1
Artyom Rudakov <two_dollars@icloud.com>
2025-03-23 16:18:50 +0200

    Implements basic spacial hash

--- LatropPhysics/src/collision/CollisionWorld.cpp
+++ LatropPhysics/src/collision/CollisionWorld.cpp
@@ -3,6 +3,34 @@
 
 using namespace LP;
 
+// MARK: Spacial Partitioning
+int computeCellKey(const glm::vec3& position, float cellSize) {
+    int x = static_cast<int>(position.x / cellSize);
+    int y = static_cast<int>(position.y / cellSize);
+    int z = static_cast<int>(position.z / cellSize);
+    return (x * 73856093) ^ (y * 19349663) ^ (z * 83492791);
+};
+
+void CollisionWorld::updateSpacialPartitioningOfStaticBodies(float cellSize)
+{
+    // spatialHash.clear();
+    m_cellSize = cellSize;
+
+    for(const std::weak_ptr<CollisionBody>& bodyWeakPtr : m_bodies)
+    {
+        auto bodyLocked = bodyWeakPtr.lock();
+        if (!bodyLocked) continue;
+        auto body = bodyLocked.get();
+
+        int hash = computeCellKey(body->transform.position, m_cellSize);
+        spatialHash[hash].push_back(bodyWeakPtr);
+    }
+
+    m_bodies.clear();
+}
+
+// MARK: Collision Handling
+
 template <typename A, typename B>
 inline void detectInvidualCollisionsIn(
     const std::vector<A>& lhs,
@@ -50,7 +78,19 @@ inline void detectInvidualCollisionsIn(
 void CollisionWorld::detectCollisions(std::vector<Collision>& collisions, std::vector<Collision>& triggers)
 {
     // Step 1: All Static VS All Movable
-    detectInvidualCollisionsIn(m_bodies, movableBodies, collisions, triggers);
+    // detectInvidualCollisionsIn(m_bodies, movableBodies, collisions, triggers);
+    for(const std::weak_ptr<RigidBody>& bodyWeakPtr : movableBodies)
+    {
+        auto bodyLocked = bodyWeakPtr.lock();
+        if (!bodyLocked) continue;
+        auto body = bodyLocked.get();
+
+        int hash = computeCellKey(body->transform.position, m_cellSize);
+        auto cell = spatialHash[hash];
+
+        // TODO: Extract inner loop from this function
+        detectInvidualCollisionsIn(cell, movableBodies, collisions, triggers);
+    }
 
     // Step 2: All Movable VS All Movable
     detectInvidualCollisionsIn(movableBodies, movableBodies, collisions, triggers);
