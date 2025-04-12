1a19ced2aa67b33a8900ec7942c51798155a2af5
Artyom Rudakov <two_dollars@icloud.com>
2025-04-12 21:25:36 +0300

    Adjusts getInvInertiaTensor to return the correct inertia tensor

--- LatropPhysics/include/dynamics/RigidBody.h
+++ LatropPhysics/include/dynamics/RigidBody.h
@@ -26,8 +26,10 @@ namespace LP
 
         inline float getMass() const { return m_mass; }
         inline float getInvMass() const { return m_invMass; }
-        inline glm::mat3 getInertiaTensor() const { return m_inertiaTensorLocal; }
-        inline glm::mat3 getInvInertiaTensor() const { return m_invInertiaTensorLocal; }
+        inline glm::mat3 getInvInertiaTensor() const { 
+            const glm::mat3 rotation = glm::mat3_cast(transform.rotation);
+            return rotation * m_invInertiaTensorLocal * glm::transpose(rotation);
+        }
 
         virtual void setIsSimulated(bool newValue) override
         { 
@@ -49,7 +51,6 @@ namespace LP
 
             // Recompute inertia tensor for a unit cube
             float inertia = (1.0f / 12.0f) * m_mass * (1 * 1 + 1 * 1); // For a unit cube
-            m_inertiaTensorLocal = glm::mat3(inertia);
             
             assert(inertia > 0.0f && "Inertia must be greater than zero.");
             m_invInertiaTensorLocal = glm::mat3(1.0f / inertia);
@@ -59,7 +60,6 @@ namespace LP
         float m_mass = 1.0f;
         float m_invMass = 1.0f;
 
-        glm::mat3 m_inertiaTensorLocal = glm::mat3(1.0f);
         glm::mat3 m_invInertiaTensorLocal = glm::mat3(1.0f);
     };
 }
