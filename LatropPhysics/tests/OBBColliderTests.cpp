f5c196d6b9fc6696edb9dfb2d63c4b1933fafd9c
Artyom Rudakov <two_dollars@icloud.com>
2025-04-13 21:43:35 +0300

    Updates tests

--- LatropPhysics/tests/OBBColliderTests.cpp
+++ LatropPhysics/tests/OBBColliderTests.cpp
@@ -30,9 +30,22 @@ TEST(OBBColliderTests, ATopCollision4Points) {
     ContactManifold manifold = cubeA.testCollision(&transformA, &cubeB, &transformB);
 
     ASSERT_EQ(manifold.contactsCount, 4);
-    EXPECT_NEAR(manifold.depth, 0.004999, 0.000001);
+    EXPECT_NEAR(manifold.depth, 0.004999f, 0.000001f);
     EXPECT_TRUE(Vec3Equal(manifold.contactPoints[0], { 0.4f, 0.495f, -0.5f }));
     EXPECT_TRUE(Vec3Equal(manifold.contactPoints[1], { 0.4f, 0.495f, 0.5f }));
     EXPECT_TRUE(Vec3Equal(manifold.contactPoints[2], { 0.5f, 0.5f,  -0.5f }));
     EXPECT_TRUE(Vec3Equal(manifold.contactPoints[3], { 0.5f, 0.5f,  0.5f }));
+}
+
+TEST(OBBColliderTests, ATopContact4Points) {
+    OBBCollider cubeA = OBBCollider::makeCube();
+    OBBCollider cubeB = OBBCollider::makeCube();
+    
+    Transform transformA, transformB;
+    transformB.position = glm::vec3(0.9f, 0.995f, 0.0f);
+
+    ContactManifold manifold = cubeA.testCollision(&transformA, &cubeB, &transformB);
+
+    ASSERT_EQ(manifold.contactsCount, 4);
+    EXPECT_NEAR(manifold.depth, 0.0f, 0.1f);
 }
