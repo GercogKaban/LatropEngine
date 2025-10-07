e77c75d550483b1922f8f6f187fafdd9e9268128
Artyom Rudakov <two_dollars@icloud.com>
2025-10-07 21:30:16 +0300

    Updates sky-box color

--- LatropGraphics/src/LRenderer.cpp
+++ LatropGraphics/src/LRenderer.cpp
@@ -2364,7 +2364,7 @@ void LRenderer::RenderPass::beginPass(VkCommandBuffer commandBuffer, VkFramebuff
     renderPassInfo.renderArea.extent = size;
 
     std::array<VkClearValue, 2> clearValues{};
-    clearValues[0].color = { {0.0f, 0.0f, 0.0f, 1.0f} };
+    clearValues[0].color = { { 0.486, 0.78, 0.91, 1.0f } };
     clearValues[1].depthStencil = { 1.0f, 0 };
 
     renderPassInfo.clearValueCount = static_cast<uint32>(clearValues.size());
