89823ff4507a5a68784bb764c3ba65f806601355
Artyom Rudakov <two_dollars@icloud.com>
2025-10-09 21:31:35 +0300

    Updates symmetric sample

--- src/scenes/PortalsSandbox.cpp
+++ src/scenes/PortalsSandbox.cpp
@@ -10,66 +10,10 @@ void PortalsSandbox::createSymmetricScenario()
     RoomScene::createFloor(400, 400);
 
     // MARK: Cubes
-    bool isSimulated = false;
-    auto cubeA = ObjectBuilder::construct<LActor>().lock();
-	cubeA->loadComponent<LG::LCube>();
-    cubeA->graphicsComponent->setColorTexture("textures/smile2.jpg");
-	cubeA->loadComponent< LP::RigidBody>([isSimulated](LP::RigidBody* physicsComponent)
-		{
-			physicsComponent->setIsSimulated(isSimulated);
-			if (isSimulated) physicsComponent->setMass(10.0f);
-
-			physicsComponent->collider = cubeOBBCollider;
-			physicsComponent->takesGravity = isSimulated;
-			physicsComponent->material = LP::Material::Metal;
-			physicsComponent->material.restitution = 1.0f;
-			physicsComponent->transform.position = glm::vec3(6.0f, 1.0f, -2.0f);
-		});
-
-    auto cubeB = ObjectBuilder::construct<LActor>().lock();
-	cubeB->loadComponent<LG::LCube>();
-    cubeB->graphicsComponent->setColorTexture("textures/smile2.jpg");
-	cubeB->loadComponent< LP::RigidBody>([isSimulated](LP::RigidBody* physicsComponent)
-		{
-			physicsComponent->setIsSimulated(isSimulated);
-			if (isSimulated) physicsComponent->setMass(10.0f);
-
-			physicsComponent->collider = cubeOBBCollider;
-			physicsComponent->takesGravity = isSimulated;
-			physicsComponent->material = LP::Material::Metal;
-			physicsComponent->material.restitution = 1.0f;
-			physicsComponent->transform.position = glm::vec3(3.0f, 1.0f, -1.0f);
-		});
-
-    auto cubeC = ObjectBuilder::construct<LActor>().lock();
-	cubeC->loadComponent<LG::LCube>();
-    cubeC->graphicsComponent->setColorTexture("textures/smile2.jpg");
-	cubeC->loadComponent< LP::RigidBody>([isSimulated](LP::RigidBody* physicsComponent)
-		{
-			physicsComponent->setIsSimulated(isSimulated);
-			if (isSimulated) physicsComponent->setMass(10.0f);
-
-			physicsComponent->collider = cubeOBBCollider;
-			physicsComponent->takesGravity = isSimulated;
-			physicsComponent->material = LP::Material::Metal;
-			physicsComponent->material.restitution = 1.0f;
-			physicsComponent->transform.position = glm::vec3(-3.0f, 1.0f, -9.0f);
-		});
-
-    auto cubeD = ObjectBuilder::construct<LActor>().lock();
-	cubeD->loadComponent<LG::LCube>();
-    cubeD->graphicsComponent->setColorTexture("textures/smile2.jpg");
-	cubeD->loadComponent< LP::RigidBody>([isSimulated](LP::RigidBody* physicsComponent)
-		{
-			physicsComponent->setIsSimulated(isSimulated);
-			if (isSimulated) physicsComponent->setMass(10.0f);
-
-			physicsComponent->collider = cubeOBBCollider;
-			physicsComponent->takesGravity = isSimulated;
-			physicsComponent->material = LP::Material::Metal;
-			physicsComponent->material.restitution = 1.0f;
-			physicsComponent->transform.position = glm::vec3(-6.0f, 1.0f, -8.0f);
-		});
+	SharedScene::createCube({ 6.0f, 1.0f, -2.0f }, false);
+	SharedScene::createCube({ 3.0f, 1.0f, -1.0f }, false);
+	SharedScene::createCube({ -3.0f, 1.0f, -9.0f }, false);
+	SharedScene::createCube({ -6.0f, 1.0f, -8.0f }, false);
 
     // MARK: Portals
     const glm::vec3 portalScale = glm::vec3(1.6f, 0.01f, 2.8f);
@@ -79,11 +23,11 @@ void PortalsSandbox::createSymmetricScenario()
 	const glm::vec3 pos2 = glm::vec3(-4.0f, 1.5f, -5.0f);
 
 	auto bluePortal = ObjectBuilder::construct<LActor>().lock();
-	bluePortal->loadComponent<LG::LPortal>([pos1](LG::LGraphicsComponent* graphicsComponent)
+	bluePortal->loadComponent<LG::BluePortal>([pos1](LG::LGraphicsComponent* graphicsComponent)
 		{
 			const glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, 1.0f);
 			const glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-			dynamic_cast<LG::LPortal*>(graphicsComponent)->setPortalView(glm::lookAt(pos1, pos1 + cameraFront, cameraUp));
+			dynamic_cast<LG::BluePortal*>(graphicsComponent)->setPortalView(glm::lookAt(pos1, pos1 + cameraFront, cameraUp));
 
 			auto p1View = glm::lookAt(pos1, pos1 + cameraFront, cameraUp);
 
@@ -110,11 +54,11 @@ void PortalsSandbox::createSymmetricScenario()
 		});
 
 	auto orangePortal = ObjectBuilder::construct<LActor>().lock();
-	orangePortal->loadComponent<LG::LPortal>([pos2](LG::LGraphicsComponent* graphicsComponent)
+	orangePortal->loadComponent<LG::OrangePortal>([pos2](LG::LGraphicsComponent* graphicsComponent)
 		{
 			const glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, 1.0f);
 			const glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-			dynamic_cast<LG::LPortal*>(graphicsComponent)->setPortalView(glm::lookAt(pos2, pos2 + cameraFront, cameraUp));
+			dynamic_cast<LG::OrangePortal*>(graphicsComponent)->setPortalView(glm::lookAt(pos2, pos2 + cameraFront, cameraUp));
 		});
 	orangePortal->loadComponent<LP::RigidBody>([pos2, portalScale, rotationY1](LP::RigidBody* physicsComponent)
 		{
