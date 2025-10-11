d8834085465f5d87c780b9a82a684b4995939077
Artyom Rudakov <two_dollars@icloud.com>
2025-10-12 02:27:43 +0300

    Adjusts portals scale

--- include/scenes/Shared.h
+++ include/scenes/Shared.h
@@ -12,6 +12,6 @@ namespace SharedScene
     void createPlayer(glm::vec3 origin = glm::vec3(2.0f, 2.0f, 2.0f), bool takesGravity = true);
 
     void createCube(glm::vec3 origin, bool takesGravity = true, std::string &&texturePath = "textures/smile2.jpg");
-    void createBluePortal(glm::vec3 origin, glm::quat rotation = glm::angleAxis(glm::radians(0.0f), glm::vec3(0, 0, 1)), glm::vec3 scale = { 1.6f, 0.3f, 2.8f }, bool isDecorated = false);
-    void createOrangePortal(glm::vec3 origin, glm::quat rotation = glm::angleAxis(glm::radians(0.0f), glm::vec3(0, 0, 1)), glm::vec3 scale = { 1.6f, 0.3f, 2.8f }, bool isDecorated = false);
+    void createBluePortal(glm::vec3 origin, glm::quat rotation = glm::angleAxis(glm::radians(0.0f), glm::vec3(0, 0, 1)), glm::vec3 scale = { 1.6f, 0.4f, 2.8f }, bool isDecorated = false);
+    void createOrangePortal(glm::vec3 origin, glm::quat rotation = glm::angleAxis(glm::radians(0.0f), glm::vec3(0, 0, 1)), glm::vec3 scale = { 1.6f, 0.4f, 2.8f }, bool isDecorated = false);
 } // namespace SharedScene
